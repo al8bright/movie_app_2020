415807b6609590dbdecda7bc3f36646ac8814979
al8bright <al8bright@gmail.com>
2020-06-03 21:14:59 +0900

    food

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food() {
+  return <h1>I like potato</h1>;
+}
 
 function App() {
   /*return <div className="App" />;*/
   return (
   <div>
     <h1>hello!!</h1>
-    <Potato />
+    <Food fav="test"/>
   </div>
   )
 }
