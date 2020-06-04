0b14f7cef142d61317c7f1de61dfe7460a65f384
al8bright <al8bright@gmail.com>
2020-06-05 01:23:59 +0900

    rating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Food({name, picture}) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture}/>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
   );
 }
@@ -44,13 +46,14 @@ const foodILike = [
   },
 ];
 
+
 function App() {
-  /*return <div className="App" />;*/
   return (
   <div>
-    {foodILike.map(dish => 
-      (<Food name={dish.name} picture={dish.image} />)
-    )}
+    {foodILike.map (dish => 
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      )
+    }
   </div>
   )
 }
