c96834cc6febd6dc1a8df87255c4b6e20c3de8f5
al8bright <al8bright@gmail.com>
2020-06-13 22:32:59 +0900

    Updates

---
+++ precache-manifest.6c0c78b68eaee27acc749ca56376d8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be0f4913a26b43da97b7937f0ce1e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "364a2eefef2762ca091a",
+    "url": "/static/css/main.9c61af52.chunk.css"
+  },
+  {
+    "revision": "4c0e25f7832ea0655892",
+    "url": "/static/js/2.eda0c3c0.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.eda0c3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364a2eefef2762ca091a",
+    "url": "/static/js/main.51f78b3c.chunk.js"
+  },
+  {
+    "revision": "52f8b573bb3b2c0ff873",
+    "url": "/static/js/runtime-main.592242c7.js"
+  }
+]);
